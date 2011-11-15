6772c5a96eecbabf8c2acbcc498cd57eec5294bd
Arnvald <grzegorz.witek@netguru.pl>
2011-11-15 17:23:40 +0100

    show top_page banner if exists

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
 
   def index
     @offers = Offer.last(6)
-    @contract = Contract.first
+    @contract_top = Contract.active.of_placement(Placement.find_by_name("top_page")).first
   end
 
 end
