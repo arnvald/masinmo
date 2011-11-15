4ff92896152c8226da9e02c0f93a101c5b0a5fad
Arnvald <grzegorz.witek@netguru.pl>
2011-11-15 16:45:58 +0100

    temporarily hide users and offers in admin

--- app/admin/offers.rb
+++ app/admin/offers.rb
@@ -1,20 +1,20 @@
-ActiveAdmin.register Offer do
-  scope :all, default: true
-  scope :drafts
-  scope :published
-
-  filter :title
-  filter :user
-  filter :city
-  filter :country
-  filter :state
-
-  index do
-    column :title
-    column :user
-    column :city
-    column :state
-    column :country
-    default_actions
-  end
-end
+#ActiveAdmin.register Offer do
+#  scope :all, default: true
+#  scope :drafts
+#  scope :published
+#
+#  filter :title
+#  filter :user
+#  filter :city
+#  filter :country
+#  filter :state
+#
+#  index do
+#    column :title
+#    column :user
+#    column :city
+#    column :state
+#    column :country
+#    default_actions
+#  end
+#end
