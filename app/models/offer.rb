3562ea18a1a506fef55d66c1d9517a74be3e80e7
Arnvald <grzegorz.witek@netguru.pl>
2011-11-22 18:51:32 +0100

    customize offer view on map

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -36,7 +36,7 @@ class Offer < ActiveRecord::Base
   end
 
   def gmaps4rails_infowindow
-    "<h3>#{title}</h3> <img src = '#{photos.first.image.thumb.url}' /> <p>#{summary}</p>"
+    "<h3><a id='offer_#{id}' href='#'>#{title}</a></h3> <img src = '#{photos.first.image.thumb.url}' /> <p>#{summary}</p><p>#{gmaps4rails_address}</p><p>#{price}</p>"
   end
 
   def owned_by?(user)
