b3b833f6ebb64cd2593bd2c1832d9298fda188ac
Arnvald <grzegorz.witek@netguru.pl>
2011-11-29 23:46:56 +0100

    moved search form and added EUR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout "application"
+
+  before_filter :search_object
+
+  def search_object
+    @search = Offer.search
+  end
 end
