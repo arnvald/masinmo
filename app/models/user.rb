460bd5ab19119c0900e0662c06df8bd5ff85eb14
Arnvald <grzegorz.witek@netguru.pl>
2011-12-06 00:46:45 +0100

    user has favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   has_many :offers
   has_many :favorites, dependent: :destroy
+  has_many :favorite_offers, source: :offer, through: :favorites
   has_many :comments, dependent: :nullify
 
   def is_admin?
