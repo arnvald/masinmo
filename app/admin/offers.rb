c2f64676358fecb595b459387c13464d3ee62503
Arnvald <grzegorz.witek@netguru.pl>
2011-12-07 02:47:41 +0100

    offers in admin uncommented

--- app/admin/offers.rb
+++ app/admin/offers.rb
@@ -1,20 +1,23 @@
-#ActiveAdmin.register Offer do
-#  scope :all, default: true
-#  scope :drafts
-#  scope :published
-#
-#  filter :title
-#  filter :user
-#  filter :city
-#  filter :country
-#  filter :state
-#
-#  index do
-#    column :title
-#    column :user
-#    column :city
-#    column :state
-#    column :country
-#    default_actions
-#  end
-#end
+ActiveAdmin.register Offer do
+  scope :all, default: true
+  scope :drafts
+  scope :published
+
+  filter :title
+  filter :user
+  filter :city
+  filter :country
+  filter :state
+  filter :created_at
+  filter :expiry_date
+
+  index do
+    column :title
+    column :user
+    column :city
+    column :state
+    column :country
+    column :expiry_date
+    default_actions
+  end
+end
