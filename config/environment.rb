c85fc38ff7bf9035ae2d0d13e5f844a4cc3afefb
Arnvald <grzegorz.witek@netguru.pl>
2012-01-18 20:16:22 +0100

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Inmo::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp  
+ActionMailer::Base.raise_delivery_errors = true  
+ActionMailer::Base.default_charset = 'utf-8'  
+ActionMailer::Base.smtp_settings = { 
+  :enable_starttls_auto => true,
+  :address => 'inmo.megiteam.pl',
+  :port => 25, 
+  :user_name => 'inmo@inmo.megiteam.pl',
+  :password => 'tuneup2011',
+  :domain => 'inmo.megiteam.pl',
+  :authentication => :login 
+}
