fa33abdc345d354ea940585311b8fead01ae4e0c
Arnvald <grzegorz.witek@netguru.pl>
2012-01-19 18:11:06 +0100

    correct mail templates

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Inmo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_mailer.default_url_options = { :host => "inmo.megiteam.pl" }
   end
 end
